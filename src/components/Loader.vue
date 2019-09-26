73b33b9512c1b2a332d3dedba23169c7581666b5
Alex McKinnon <alex.r.mckinnon@gmail.com>
2019-09-26 01:49:07 -0700

    Import three-dots css within Loader component

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="loader dot-spin"></div>
 </template>
-<style>
+
+<style scoped>
+@import '../../node_modules/three-dots/dist/three-dots.min.css';
+
 .loader {
     margin: 30px auto;
 }
