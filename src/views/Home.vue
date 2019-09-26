0f4198622bcb699e4ad8619d9ea4757656f23347
Alex McKinnon <alex.r.mckinnon@gmail.com>
2019-09-26 02:01:36 -0700

    Update Home view to retrieve developer profile data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,52 @@
 <template>
-  <div class="home">
-    <Profile :email="email"/>
-  </div>
+    <div>
+        <Loader v-if="loading"/>
+        <Profile v-else :user="user"/>
+    </div>
 </template>
 
 <script>
+import Loader from '@/components/Loader.vue'
 import Profile from '@/components/Profile.vue'
 import client from '@/utils/zafclient.js'
+import axios from 'axios'
 
 export default {
     components: {
+        Loader,
         Profile
     },
     data() {
         return {
-            email: ''
+            apiUrl: '',
+            user: {},
+            loading: true
         }
     },
     mounted() {
         client.get('ticket.requester.email').then((data) => {
-            this.email = data.ticket.requester.email;
+            let email = data['ticket.requester.email'];
+            this.getProfileData(email);
         });
+    },
+    methods: {
+        getProfileData(email) {
+            try {
+                axios.get(apiUrl + email, {
+                    validateStatus: false,
+                    headers: {'Access-Control-Allow-Origin': '*'}
+                }).then(response => {
+                    this.user = {
+                        ...response.data.data
+                    }
+                })
+            } catch (err) {
+                // eslint-disable-next-line no-console
+                console.log(err);
+            } finally {
+                this.loading = false;
+            }
+        }
     }
 }
 </script>
