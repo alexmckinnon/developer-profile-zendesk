0e912b62e8ff0e0da3ee410dc3350b32dfcca465
Alex McKinnon <alex.r.mckinnon@gmail.com>
2019-09-26 01:19:40 -0700

    Add Profile component

---
+++ src/components/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span v-if="!user">No developer profile found for this requester.</span>
+    <template v-else> 
+        <span>{{ user.email }}</span>
+        <img v-if="user.github" src="/github_octocat.png">
+        <img v-if="user.linkedin" src="/linkedin.png">
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
